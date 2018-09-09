0c6b9426e728b6cbc5faba6395b56f7b3d07c78b
Rajan Khullar <rkhullar03@gmail.com>
2018-09-09 18:21:24 -0400

    adding conditional branching

--- api/lambda.py
+++ api/lambda.py
@@ -2,16 +2,51 @@ from typing import Dict
 import json
 
 
-def build_response(data: Dict=dict(), status: int=200, encoded: bool=False, headers: Dict=dict(), **kwargs):
-    combined = {**data, **kwargs}
-    return dict(body=json.dumps(combined), statusCode=status, headers=headers, isBase64Encoded=encoded)
+def build_response(data: Dict=None, status: int=200, encoded: bool=False, headers: Dict=None, **kwargs):
+    combined = {**(data or dict()), **kwargs}
+    return dict(body=json.dumps(combined), statusCode=status, headers=headers or dict(), isBase64Encoded=encoded)
 
 
 def handler(event, context):
-    method, path = event.get('httpMethod').lower(), event.get('path')
-    response = build_response(
-        message='hello world', method=method, path=path,
-        path_params=event.get('pathParameters'),
-        body=json.loads(event.get('body') or '{}')
-        )
-    return response
+    method = event['httpMethod'].lower()
+    resource, path, path_params = map(event.get, ['resource', 'path', 'pathParameters'])
+    body = json.loads(event.get('body') or '{}')
+
+    data = {}
+
+    if resource == '/products':
+        if method == 'get':
+            data['message'] = "returning all products"
+
+        elif method == 'put':
+            data['message'] = "creating new product"
+            data['name'] = body['name']
+            data['unit_price'] = body['unit_price']
+
+    elif resource == '/budgets':
+        if method == 'get':
+            data['message'] = "returning all budgets"
+
+        elif method == 'put':
+            data['message'] = "creating new budget"
+            data['name'] = body['name']
+
+    elif resource == '/products/{name+}':
+        data['name'] = path_params['name']
+
+        if method == 'get':
+            data['message'] = "returning product detail"
+
+        elif method == 'delete':
+            data['message'] = "deleting product"
+
+    elif resource == '/budgets/{name+}':
+        data['name'] = path_params['name']
+
+        if method == 'get':
+            data['message'] = "returning budget detail"
+
+        elif method == 'delete':
+            data['message'] = "deleting budget"
+
+    return build_response(data)
